81ade0db5375f21479063753571ae9dd9de4d1d9
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-07-01 11:48:57 +0600

    project is not ready yet

--- src/Component/LayOut.js
+++ src/Component/LayOut.js
@@ -5,7 +5,7 @@ const LayOut = () => {
     return (
         <div>
             <div className="bg-neutral flex justify-between items-center px-12">
-                <div><h1 className="text-accent text-4xl text-center font-bold">Hello Prithibi</h1></div>
+                <div><h1 className="text-accent text-4xl text-center font-bold">Power Hack</h1></div>
                 <div>
                     <button className="btn btn-warning btn-sm">click me!</button>
                 </div>
